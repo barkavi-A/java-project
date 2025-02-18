e1a0892e5aa9ff082d9df0f5a3bd6866ed983f0f
barkavi-A <brkavi2006@gmail.com>
2025-02-18 22:47:46 +0530

    remove command line

--- day 8/star.java
+++ day 8/star.java
@@ -1,12 +1,11 @@
 public class star {
     public static void main(String args[]){
-        int rows = 5;  // You can change the number of rows
+        int rows = 5;  
         for (int i = 1; i <= rows; i++) {
-            // Inner loop to print stars for each row
+            
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
-            // Move to the next line after printing stars for the row
             System.out.println();
         }
     }
