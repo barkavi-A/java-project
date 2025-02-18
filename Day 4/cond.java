e301c4e9409e0fa66fbe59001db5d97a3d0d2b04
barkavi-A <brkavi2006@gmail.com>
2025-02-18 21:19:02 +0530

    command line

--- Day 4/cond.java
+++ Day 4/cond.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class cond {
     public static void main(String[] args) {
        
-        Scanner scan=new Scanner(System.in);
+        Scanner scan=new Scanner(System.in);// to get input from user
         System.out.print("enter number");
         int num=scan.nextInt();
     if(num% 2==0){
