229f91c7bc64c28a784a5b169cbf6a05baed4036
barkavi-A <brkavi2006@gmail.com>
2025-02-23 21:26:10 +0530

    Add files via upload

---
+++ day 10/problem.java
@@ -0,0 +1,107 @@
+import java.util.Scanner;
+public class problem {
+    public static void main(String[] args) {
+        Scanner br = new Scanner(System.in);
+        // System.out.println("enter the celsius: ");
+        // int celsius = br.nextInt();
+        // double fahrenhit = (celsius * 9/5)+32;
+        // System.out.println("fahrenhit:"+fahrenhit);
+
+
+
+        // System.out.print("radius:");
+        // int radius = br.nextInt();
+        // double area =( 3.14 * (radius^2));
+        // System.out.println("area="+area);
+
+
+        // System.out.print("num:");
+        // int num = br.nextInt();
+        // if(num%2==0){
+        //     System.out.println("even");
+        // }else{
+        //     System.out.print("odd");
+        // }
+
+
+
+        // for(int i=1;i<=10;i++)
+        // {
+        //     System.out.println("3 *"+i + "=" +(i*3));
+        // }
+        
+
+
+
+      // arithmetic operation
+    //   int num1=10;
+    //   int num2=20;
+    //   int sum=num1+num2;
+    //   System.out.println("sum:"+sum);
+
+// floating as result
+
+    // float num1=0.01f;
+    // float num2= 0.002f;
+    // double result=num1 + num2;
+    // System.out.print("result:"+result);
+
+
+
+ // sum of 2 number
+
+
+
+    // int sum1 = br.nextInt();
+    // int sum2 = br.nextInt();
+    // int sum = sum1+ sum2;
+    // System.out.println("the sum of value:"+sum);
+
+
+
+  // swapping of three variables without using 4th variable
+
+   //int a =2,b = 3,c= 4;
+    //System.out.print( "before:a ="+ a +",b = "+ b +",c = "+c);
+    //a = a+b+c;
+    //b=a-(b+c) ;
+    //c=a-(b+c);
+    //a=a-(b+c);
+    //System.out.print("after:a="+ a +",b="+b+",c="+c);
+
+
+    // ascii of X
+
+    // char ch = 'B';
+    // int asciivalue = (int) ch;
+    // System.out.println("the ascii value of'"+ ch +"'is :"+ asciivalue);
+
+    //declare char
+
+    // char ch = 'S';
+    // System.out.println("print :"+ch);
+
+
+    //boolean expression
+   System.out.println("name:");
+   String name = br.nextLine();
+
+
+    if(name.equals("available")){
+      System.out.print("available");
+    }else{
+      System.out.print("not available");
+    }
+
+
+    
+   
+
+
+
+
+
+
+    }
+
+}
