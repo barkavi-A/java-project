21c73d980674f4d75d0db5440751fabb1343a444
barkavi-A <brkavi2006@gmail.com>
2025-03-18 22:25:40 +0530

    calculate the even days in month

---
+++ month/month.java
@@ -0,0 +1,18 @@
+
+    public class month {
+        public static void main(String[] args) {
+            int count=0;
+            for(int i=1;i<=31;i++){
+              if(i%2==0){
+                count ++;
+              }
+              else{ 
+                continue;}
+            }
+            System.out.println("count:"+count);
+        }
+    
+    }
+    
+
+
